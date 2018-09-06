6c44da7b54dac1f83184045ee02303e86f81942c
kali_ey <guoyouworld@163.com>
2018-09-06 16:33:23 +0800

    add model dz-shop-wechat

---
+++ dz-shop/dz-shop-appweb/src/main/java/com/dz/controller/LoginController.java
@@ -0,0 +1,33 @@
+package com.dz.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.junit.runner.RunWith;
+import org.springframework.stereotype.Controller;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@Controller
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({ "classpath:spring/spring-dao.xml", "classpath:spring/spring-service.xml" })
+public class LoginController {
+	
+	
+	@RequestMapping(value="/auth/login", method = RequestMethod.POST)
+	@ResponseBody
+	public String test(HttpServletRequest request ,Model model) {
+		Object username = request.getParameter("username");
+		Object password = request.getParameter("password");
+		
+		System.out.println(username);
+		System.out.println(password);
+		return "success";
+	}
+}
