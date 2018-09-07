3add1f2bc0dccfcd3be17b42183b067fcba7f575
kali_ey <guoyouworld@163.com>
2018-09-07 14:44:05 +0800

    fix post param bug

--- dz-shop/dz-shop-appweb/src/main/java/com/dz/controller/LoginController.java
+++ dz-shop/dz-shop-appweb/src/main/java/com/dz/controller/LoginController.java
@@ -8,19 +8,20 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
+//@CrossOrigin(origins = "*", maxAge = 3600)
 @Controller
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({ "classpath:spring/spring-dao.xml", "classpath:spring/spring-service.xml" })
 public class LoginController {
 	
 	
-	@RequestMapping(value="/auth/login", method = RequestMethod.GET)
+	@RequestMapping(value="/auth/login", method = RequestMethod.POST)
 	@ResponseBody
 	public String test(HttpServletRequest request ,Model model) {
 		Object username = request.getParameter("username");
@@ -30,24 +31,17 @@ public class LoginController {
 		System.out.println(password);
 		return "success";
 	}
-	@RequestMapping(value="auth/login_by_weixin", method = RequestMethod.GET)
+	@RequestMapping(value="/auth/login_by_weixin", method = RequestMethod.POST)
 	@ResponseBody
-	public String login_by_weixin(HttpServletRequest request ,Model model) {
-		Object username = request.getParameter("username");
-		Object password = request.getParameter("password");
+	public String login_by_weixin(HttpServletRequest request ,Model model,@RequestBody String param ) {
+		//因为'Content-Type': 'application/json',，所以request.getParameter这种方式无法获取值
+		//@RequestParam用来处理ContentType: 为 application/x-www-form-urlencoded编码的内容
+		//@RequestBody该注解常用来处理Content-Type: 不是application/x-www-form-urlencoded编码的内容，例如application/json, application/xml等；
 		
-		System.out.println(username);
-		System.out.println(password);
-		return "success";
-	}
-	@RequestMapping(value="/auth/login", method = RequestMethod.GET)
-	@ResponseBody
-	public String test(HttpServletRequest request ,Model model) {
-		Object username = request.getParameter("username");
-		Object password = request.getParameter("password");
+		//Object code = request.getParameter("code");
+		//Object userInfo = request.getParameter("userInfo");
 		
-		System.out.println(username);
-		System.out.println(password);
+		System.out.println(param);
 		return "success";
 	}
 }
